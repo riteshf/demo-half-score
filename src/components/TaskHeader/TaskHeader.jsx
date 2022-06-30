021300e5d861f5449904a1cf9a9c6f36e5b741f0
Ritesh Firodiya <ritesh@Riteshs-MacBook-Pro.local>
2022-06-30 12:51:49 +0530

    low scorew

--- src/components/TaskHeader/TaskHeader.jsx
+++ src/components/TaskHeader/TaskHeader.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import styles from "./taskHeader.module.css";
 
 const TaskHeader = ({ tasks }) => {
-  let remainingTasks = tasks.filter((task) => !task.done).length;
-
   return (
     <div data-testid="task-header" className={styles.taskHeader}>
       <h1>Todo List</h1>
 
       <small>
-        You have <b data-testid="header-remaining-task">{remainingTasks}</b> of{" "}
+        You have <b data-testid="header-remaining-task">{1}</b> of{" "}
         <b data-testid="header-total-task">{tasks.length}</b> tasks remaining
       </small>
     </div>
